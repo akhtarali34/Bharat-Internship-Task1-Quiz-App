98bccd22d8364ffab290ace601b3090218262d29
abbasahmed34 <abbasahmed600@gmail.com>
2024-01-30 09:58:10 +0500

    full Quiz App

---
+++ lib/data/questions.dart
@@ -0,0 +1,40 @@
+import 'package:quiz_app/model/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Pakistan independance year is? ',
+    [
+      '1947',
+      '1948',
+      '1949',
+      '1950',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the current caretaker Prime minister of Pakistan ? ',
+    [
+      'Anwaarul Haq Kakar',
+      'Ishaq dar',
+      'Jalil Abbas jilani',
+      'None of These',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the Current Chief Minister of SINDH?? ',
+    [
+      'Justice (r) Maqbool Baqir (caretaker)',
+      'Syed Murad Ali Shah',
+      'Qaim Ali Shah',
+      'Zahid Qurban Alvi',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the Current IG of Sindh Police? ',
+    [
+      'Riffat Mukhtar Raja',
+      'Mushtaq Mehar',
+      'Ghulam Nabi Memon',
+      'None of These',
+    ],
+  ),
+];
